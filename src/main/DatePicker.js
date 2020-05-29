070ef137f6047e9c32d893d1e2ed3b6cf75cf075
ZachyDev <mosetizachary001@gmail.com>
2020-05-30 00:31:21 +0300

    added the datepicker component

---
+++ src/main/DatePicker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import DatePicker  from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css"
+class DatePicker extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default DatePicker
