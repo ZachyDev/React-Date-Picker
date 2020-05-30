7aa9e193a1694f6fa3b359b564dbb9a3ebb7842f
ZachyDev <mosetizachary001@gmail.com>
2020-05-30 13:50:33 +0300

    showYearDropdown and scrollableYearDropdown

--- src/main/Date.js
+++ src/main/Date.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import DatePicker  from 'react-datepicker'
 import "react-datepicker/dist/react-datepicker.css"
-
 class Date extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             selectedDate: ''
+        }
+    }
+    setSelectedDate = (date) => {
+        this.setState({
+            selectedDate: date
+        })
+    }
+    
     render() {
+        const { selectedDate } = this.state
         return (
             <div>
-                
+                <DatePicker
+                    selected = { selectedDate }
+                    onChange = { this.setSelectedDate }
+                    isClearable
+                 />
             </div>
         )
     }
